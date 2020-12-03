ab67fbce6aa61e1d7aba848cd0a5af58974d7032
Andrew Harrison <andrew@literati.com>
2020-12-02 23:14:00 -0600

    Day03, Parts 1 and 2

--- day03/day03.py
+++ day03/day03.py
@@ -3,7 +3,55 @@ class Day03:
         self.raw_data = data
 
     def solve_part1(self):
-        pass
+        grid = self.load_grid()
+        x = 3
+        y = 1
+        size = len(grid[0])
+        height = len(grid)
+        print(grid, size, height)
+
+        trees = 0
+        while y < height:
+            if grid[y][x] == "#":
+                trees += 1
+            y += 1
+            x += 3
+            if x >= size:
+                x = x - size
+        print(trees)
 
     def solve_part2(self):
-        pass
+        grid = self.load_grid()
+        x = 3
+        y = 1
+        size = len(grid[0])
+        height = len(grid)
+
+        tree_hits = []
+        for slopes in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+            trees = 0
+            x = slopes[0]
+            y = slopes[1]
+            while y < height:
+                if grid[y][x] == "#":
+                    trees += 1
+                y += slopes[1]
+                x += slopes[0]
+                if x >= size:
+                    x = x - size
+            print(trees)
+            tree_hits.append(trees)
+
+        prod = 1
+        for t in tree_hits:
+            prod *= t
+
+        print(prod)
+
+    def load_grid(self):
+
+        grid = []
+        for line in self.raw_data:
+            grid.append(list(line))
+
+        return grid
--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from fileutils import file_as_list
 
 
 def main():
-    day = "01"
+    day = "03"
     data = file_as_list(f"day{day}/day{day}input.txt")
 
     runner = day03.Day03(data)
-    runner = day01.Day01(data)
-    # runner.solve_part1()
-    runner.solve_part2_efficient2()
+
+    print("Part 1")
+    runner.solve_part1()
+
+    print("Part 2")
+    runner.solve_part2()
 
 
 if __name__ == "__main__":
